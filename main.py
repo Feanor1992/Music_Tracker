e4e5904a07b98a07f1d022a4d136401fd9cd3b6c
Feanor1992 <feanor1992@gmail.com>
2023-09-26 13:32:24 +0300

    minor changes in search_album() and add_album() functions/ add temporary main function

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ albums = pd.DataFrame(columns=['artist', 'album', 'year'])
 
 def read_albums():
     global albums
-    albums = pd.read_csv('albums.csv', dtype={'year': int})
     if albums.empty:
         albums = pd.DataFrame(columns=['artist', 'album', 'year'])
+    else:
+        albums = pd.read_csv('albums.csv', dtype={'year': int})
 
 
 def add_album():
@@ -19,8 +20,8 @@ def add_album():
     - album - album name
     - year - year of publishing"""
 
-    artist = input('Input Artist name: ')
-    album = input('Input Album name: ')
+    artist = input('Input Artist name: ').lower()
+    album = input('Input Album name: ').lower()
     year = input('Input Year of publishing: ')
 
     # check that year is a number
@@ -52,14 +53,44 @@ def add_album():
 def search_album():
     """function for album searching"""
 
-    query = input('What are you searching? ')
+    query = input('What are you searching? ').lower()
 
     # search album by the name of artist, album or publishing year
     albums_found = albums.loc[albums['album'].str.contains(query)]
     if not albums_found.empty:
         print('Found this albums: ')
         for album in albums_found.to_dict(orient='records'):
-            print(f'{album["artist"]} - {album["album"]} ({album["year"]}')
+            print(f'{album["artist"]} - {album["album"]} ({album["year"]})')
     else:
         print('Album not found')
+
+
+def main():
+    read_albums()
+    # read_books()
+
+    while True:
+        print('What you want to do?')
+        print('1. Add album')
+        # print('2. Add book')
+        print('3. Search album')
+        # print('4. Search book')
+        print('5. Exit')
+
+        choice = input()
+
+        if choice == '1':
+            add_album()
+        elif choice == '3':
+            search_album()
+        elif choice == '5':
+            break
+        else:
+            print('You must print numbers 1-5')
+            choice = input()
+
+
+if __name__ == '__main__':
+    main()
+
 
