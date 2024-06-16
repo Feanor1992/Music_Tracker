3f754af8670a0c22319c3f106401812a8b02a843
Feanor1992 <feanor1992@gmail.com>
2024-06-16 15:57:13 +0300

    add show_albums function

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import pandas as pd
 # dataframe of listened albums
 albums = pd.DataFrame(columns=['artist', 'album', 'year'])
 
-# will be using it in another app
-# dataframe of books read
-# books = pd.DataFrame(columns=['author', 'cycle', 'book'])
-
 root = tk.Tk()
 root.title('Music and Book Tracker')
 root.geometry('1240x800')
+
+table_albums = None
+add_widgets = []
+search_widgets = []
 
 
 def read_albums():
@@ -22,20 +22,19 @@ def read_albums():
     else:
         albums = pd.read_csv('albums.csv')
 
-# will be using it in another app
-# def read_books():
-#    global books
-#    if not os.path.isfile('books.xlsx'):
-#        books = pd.DataFrame(columns=['author', 'cycle', 'book'])
-#    else:
-#        books = pd.read_csv('books.csv')
-
 
 def add_album():
     """add albums to dataframe, that contains the following data
     - artist: artist name
     - album - album name
     - year - year of publishing"""
+    global table_albums, search_widgets
+
+    # Destroy previous widgets if they exist
+    if table_albums:
+        table_albums.destroy()
+    for widget in search_widgets:
+        widget.destroy()
 
     artist_label = tk.Label(root, text='Input Artist name: ')
     artist_entry = tk.Entry(root)
@@ -44,12 +43,16 @@ def add_album():
     year_label = tk.Label(root, text='Input Year of publishing: ')
     year_entry = tk.Entry(root)
 
-    artist_label.grid(row=0, column=0)
-    artist_entry.grid(row=0, column=1, columnspan=3)
-    album_label.grid(row=1, column=0)
-    album_entry.grid(row=1, column=1, columnspan=3)
-    year_label.grid(row=2, column=0)
-    year_entry.grid(row=2, column=1, columnspan=3)
+    artist_label.place(x=10, y=10)
+    artist_entry.place(x=150, y=10, width=200)
+    album_label.place(x=10, y=40)
+    album_entry.place(x=150, y=40, width=200)
+    year_label.place(x=10, y=70)
+    year_entry.place(x=150, y=70, width=200)
+
+    add_widgets.extend([artist_label, artist_entry,
+                        album_label, album_entry,
+                        year_label, year_entry])
 
     def add_album_to_csv():
         """save album to dataframe"""
@@ -65,7 +68,7 @@ def add_album():
         # Check if the album already exists in the dataframe
         if album_data['album'] in list(albums['album']):
             listened_album = tk.Label(root, text='You have listened this album yet')
-            listened_album.grid(row=4, column=0, columnspan=2)
+            listened_album.place(x=10, y=130)
             artist_entry.delete(0, tk.END)
             album_entry.delete(0, tk.END)
             year_entry.delete(0, tk.END)
@@ -80,7 +83,7 @@ def add_album():
 
         # display success message
         album_successfully_add = tk.Label(root, text=f'Album "{album}" successfully added to the list of listened albums')
-        album_successfully_add.grid(row=4, column=0, columnspan=3)
+        album_successfully_add.place(x=10, y=130)
 
         artist_entry.delete(0, tk.END)
         album_entry.delete(0, tk.END)
@@ -88,74 +91,27 @@ def add_album():
         artist_entry.focus()
 
     add_button = tk.Button(root, text='Add', command=add_album_to_csv)
-    add_button.grid(row=3, column=0, columnspan=2)
-
-
-# will be using it in another app
-# def add_book():
-#    """add books that you have read to dataframe, that contains the following data
-#    - author: author name
-#    - cycle - name of cycle to which the book belongs
-#    - book - book name"""
-#
-#    author_label = tk.Label(root, text='Input Author name: ')
-#    author_entry = tk.Entry(root)
-#    cycle_label = tk.Label(root, text='Input Cycle name: ')
-#    cycle_entry = tk.Entry(root)
-#    book_label = tk.Label(root, text='Input Book name: ')
-#    book_entry = tk.Entry(root)
-#
-#    author_label .grid(row=0, column=0)
-#    author_entry.grid(row=0, column=1, columnspan=3)
-#    cycle_label.grid(row=1, column=0)
-#    cycle_entry.grid(row=1, column=1, columnspan=3)
-#    book_label.grid(row=2, column=0)
-#    book_entry.grid(row=2, column=1, columnspan=3)
-#
-#    def add_book_to_csv():
-#        """save book to dataframe"""
-#        global books
-#
-#         author = str(author_entry.get())
-#         cycle = str(cycle_entry.get())
-#         book = str(book_entry.get())
-#
-#         # Create a Series with book information
-#         book_data = pd.Series({'author': author, 'cycle': cycle, 'book': book})
-#
-#         # Check if the book already exists in the dataframe
-#         if book_data['book'] in list(books[books['cycle'] == cycle]['book']):
-#             read_book = tk.Label(root, text='You have read this book yet')
-#             read_book.grid(row=4, column=0, columnspan=2)
-#             return
-#
-#         # Add new book to the dataframe
-#         books.loc[len(books)] = book_data
-#
-#         # save updated dataframe to the csv file
-#         books.to_csv('books.csv', index=False)
-#
-#         # display success message
-#         book_successfully_add = tk.Label(root, text=f'Book {book} successfully added to the list')
-#         book_successfully_add.grid(row=4, column=0, columnspan=3)
-#
-#         author_entry.delete(0, tk.END)
-#         cycle_entry.delete(0, tk.END)
-#         book_entry.delete(0, tk.END)
-#         author_entry.focus()
-#
-#     add_button = tk.Button(root, text='Add', command=add_book_to_csv)
-#     add_button.grid(row=3, column=0, columnspan=2)
+    add_button.place(x=10, y=100)
+    add_widgets.append(add_button)
 
 
 def search_album():
     """function for searching albums in the list"""
+    global table_albums, add_widgets
+
+    # Destroy previous widgets if they exist
+    if table_albums:
+        table_albums.destroy()
+    for widget in add_widgets:
+        widget.destroy()
 
     album_label = tk.Label(root, text='Input Album name: ')
     album_entry = tk.Entry(root)
 
-    album_label.grid(row=0, column=0)
-    album_entry.grid(row=0, column=1, columnspan=3)
+    album_label.place(x=10, y=10)
+    album_entry.place(x=150, y=10, width=200)
+
+    search_widgets.extend([album_label, album_entry])
 
     def search_album_in_df():
         album = str(album_entry.get())
@@ -164,111 +120,53 @@ def search_album():
         # Check if the album already exists in the dataframe
         if album_data['album'] in list(albums['album']):
             listened_album = tk.Label(root, text=f'You have listened {album} yet')
-            listened_album.grid(row=2, column=0, columnspan=3)
+            listened_album.place(x=10, y=70)
             album_entry.delete(0, tk.END)
             album_entry.focus()
         elif album_data['album'] not in list(albums['album']):
             listened_album = tk.Label(root, text=f'You have not listen {album}')
-            listened_album.grid(row=2, column=0, columnspan=3)
+            listened_album.place(x=10, y=70)
             album_entry.delete(0, tk.END)
             album_entry.focus()
 
     add_button = tk.Button(root, text='Search', command=search_album_in_df)
-    add_button.grid(row=1, column=0, columnspan=2)
-
-
-# will be using it in another app
-# def search_book():
-#     """function for searching books in the list"""
-#
-#     book_label = tk.Label(root, text='Input Book name: ')
-#     book_entry = tk.Entry(root)
-#
-#     book_label.grid(row=0, column=0)
-#     book_entry.grid(row=0, column=1, columnspan=3)
-#
-#     def search_book_in_df():
-#         book = str(book_entry.get())
-#         book_data = pd.Series({'abook': book})
-#
-#         # Check if the album already exists in the dataframe
-#         if book_data['book'] in list(books['book']):
-#             read_book = tk.Label(root, text=f'You have listened {book} yet')
-#             read_book.grid(row=2, column=0, columnspan=3)
-#             book_entry.delete(0, tk.END)
-#             book_entry.focus()
-#         elif book_data['book'] not in list(books['book']):
-#             read_book = tk.Label(root, text=f'You have not read {book}')
-#             read_book.grid(row=2, column=0, columnspan=3)
-#             book_entry.delete(0, tk.END)
-#             book_entry.focus()
-#
-#     add_button = tk.Button(root, text='Search', command=search_book_in_df)
-#     add_button.grid(row=1, column=0, columnspan=2)
+    add_button.place(x=10, y=40)
+    search_widgets.append(add_button)
 
 
 def show_albums():
     """function for showing albums in the list"""
+    global table_albums, add_widgets, search_widgets
+    # Destroy previous widgets if they exist
+    for widget in add_widgets:
+        widget.destroy()
+    for widget in search_widgets:
+        widget.destroy()
+    if table_albums:
+        table_albums.destroy()
 
     if not albums.empty:
-        table_albums = ttk.Treeview(root, columns=['artist', 'album', 'year'])
-        table_albums.grid(row=0, column=0, columnspan=2, sticky="nsew")
+        table_albums = ttk.Treeview(root, columns=['artist', 'album', 'year'], show='headings')
+        table_albums.place(x=10, y=10, width=1220, height=780)
 
         # add name of columns
         for column in albums.columns:
             table_albums.heading(column, text=column)
 
         # add values to table
-        for row in albums.iterrows():
-            table_albums.insert('', 'end', values=row)
+        for _, row in albums.iterrows():
+            table_albums.insert('', 'end', values=row.tolist())
+
+        # Adjust column widths to fit the window size
+        for col in table_albums['columns']:
+            table_albums.column(col, width=root.winfo_width() // len(table_albums['columns']))
+
+        # Make the table resizable
+        root.grid_rowconfigure(0, weight=1)
+        root.grid_columnconfigure(0, weight=1)
     else:
         empty_df = tk.Label(root, text='File is empty')
-        empty_df.grid(row=1, column=0, columnspan=2)
-
-
-# will be using it in another app
-# def show_books():
-#     """function for showing books in the list"""
-#
-#     if not books.empty:
-#         print('List of books: ')
-#         for book in books.sort_values(by=['author'], ascending=True).to_dict(orient='records'):
-#             print(f'{book["author"]} - {book["cycle"]}: ({book["book"]})')
-#     else:
-#         print('Books list is empty or not found')
-
-# def main():
-#    read_albums()
-#    read_books()
-#
-#    while True:
-#        print('What you want to do?')
-#        print('1. Add album')
-#        print('2. Add book')
-#        print('3. Search album')
-#        print('4. Search book')
-#        print('5. Show list of albums')
-#        print('6. Show list of books')
-#        print('7. Exit')
-#
-#        choice = input()
-#
-#        if choice == '1':
-#            add_album()
-#        elif choice == '2':
-#            add_book()
-#        elif choice == '3':
-#            search_album()
-#        elif choice == '4':
-#            search_book()
-#        elif choice == '5':
-#            show_albums()
-#        elif choice == '6':
-#            show_books()
-#        elif choice == '7':
-#            break
-#        else:
-#            print('You must print numbers 1-7')
+        empty_df.place(x=10, y=10)
 
 
 def show_gui():
@@ -276,20 +174,13 @@ def show_gui():
     file_menu = tk.Menu(menubar, tearoff=0)
     root.config(menu=file_menu)
     file_menu.add_command(label="Add Album", command=add_album)
-    # file_menu.add_command(label="Add Book", command=add_book)
     file_menu.add_command(label="Search Album", command=search_album)
-    # file_menu.add_command(label="Search Book", command=search_book)
-    # file_menu.add_command(label="Show Albums", command=show_albums)
+    file_menu.add_command(label='Show Albums', command=show_albums)
 
 
 def main():
     read_albums()
-    # read_books()
-
     show_gui()
-    # add_album()
-    # add_book()
-
     root.mainloop()
 
 
